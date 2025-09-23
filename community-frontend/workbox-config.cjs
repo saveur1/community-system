b51f6f125f1ce20dabe0cda82e818dbf45f8cebc
saveur1 <bikorimanaxavier@gmail.com>
2025-09-23 07:39:19 +0200

    cleaned a project

--- community-frontend/workbox-config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	globDirectory: 'public_html/',
-	globPatterns: [
-		'**/*.{js,css,ico,svg,jpg,png,html,json,txt}'
-	],
-	swSrc: 'scripts/src-sw.js',
-	swDest: 'public_html/sw.js'
-};
